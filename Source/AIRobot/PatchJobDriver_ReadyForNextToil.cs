d688b8c317cadec3752699d389eb679b0e8ea19d
Doug Benham <dougbenham@cox.net>
2021-12-30 22:12:05 -0700

    Added unforbid mined resources, updated package format

---
+++ Source/AIRobot/PatchJobDriver_ReadyForNextToil.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace AIRobot
+{
+    [HarmonyPatch(typeof(JobDriver), nameof(JobDriver.ReadyForNextToil))]
+    public class PatchJobDriver_ReadyForNextToil
+    {
+        // Unforbid resources that were mined outside of home area
+        static void Prefix(JobDriver __instance)
+        {
+            var pos = __instance?.job?.targetA.Cell;
+            if (pos.HasValue && pos.Value.IsValid
+                             && __instance is JobDriver_Mine
+                             && __instance.pawn?.GetType().Namespace == "AIRobot"
+                             && __instance.pawn.Map != null)
+            {
+                foreach (var forbidden in pos.Value.GetThingList(__instance.pawn.Map).Where(t => t.IsForbidden(Faction.OfPlayer)))
+                    forbidden.SetForbidden(false, false);
+            }
+        }
+    }
+}
